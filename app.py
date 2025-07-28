ffc71103df89b5b44583ac2f9407c851ffc4324b
Ning <719860478@qq.com>
2025-07-28 11:26:42 +0800

    m

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ import io
 import pandas as pd
 
 # Streamlit page configuration
-lan1, lan2 = st.columns([4, 4])
+lan1, lan2 = st.columns([4, 1], gap="small")
 with lan1: st.button("English")
 with lan2: st.button("中文")
 
