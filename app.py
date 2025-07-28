caba255510e6c143189125fec68d2f643ea49030
Ning <719860478@qq.com>
2025-07-28 11:27:27 +0800

    b

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ import io
 import pandas as pd
 
 # Streamlit page configuration
-lan1, lan2 = st.columns([4, 1], [4, 2])
+lan1, lan2 = st.columns([4, 1])
 with lan1: st.button("English")
 with lan2: st.button("中文")
 
